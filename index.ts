649a0567f0a7834728761cc35ca1315d8903ee00
Your Name <you@example.com>
2021-12-29 18:01:10 -0800

    More consistant logging

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as run from "./load.js"
 let startupFlag = process.argv[2]
 
 if (!startupFlag) {
-    console.error("No startup flag passed! Valid values are either transpile or run!")
+    console.error("[!] No startup flag passed! Valid values are either transpile or run!")
     process.exit(1)
 } else {
     startupFlag = startupFlag.toLowerCase()
@@ -12,7 +12,7 @@ if (!startupFlag) {
 
 switch(startupFlag) {
     default:
-        console.error("Unknown startup flag! Valid values are either transpile or run!")
+        console.error("[!] Unknown startup flag! Valid values are either transpile or run!")
         process.exit(1)
     case "transpile":
         await transpile.run()
